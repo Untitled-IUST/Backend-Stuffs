5a445afa4be4b25f2f488bbc916afdd6a80f22fe
Amirali Farazmand <75451772+AmiraliFarazmand@users.noreply.github.com>
2023-04-09 10:32:59 +0330

    added serializer and view to Comments. URL pattern still remains.

--- Barber/serializers.py
+++ Barber/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Barber
-
+from Auth.models import Barber as BarberModel_Auth
 
 
 class BarberSerializer(serializers.ModelSerializer):
@@ -8,4 +8,9 @@ class BarberSerializer(serializers.ModelSerializer):
         model = Barber
         fields = ['BarberShop','Owner','phone_Number','address']
     
-
+class CommentsOnBarberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BarberModel_Auth
+        fields = ("customer", "body", "barber", "created_at", "parent_comment")
+        read_only_fields = ( "created_at") 
+        
--- Barber/views.py
+++ Barber/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from rest_framework.generics import RetrieveAPIView
-from .models import Barber
+from rest_framework.generics import RetrieveAPIView, ListCreateAPIView
+from .models import Barber, Comment
+from Auth.models import Barber as BarberModel_Auth 
 from .serializers import BarberSerializer
 
 
@@ -10,5 +11,15 @@ class BarberView(RetrieveAPIView):
     queryset = Barber.objects.all()
     serializer_class = BarberSerializer
 
-
-
+# Consider other fields of 
+class BarberDetail(ListCreateAPIView):
+    model= BarberModel_Auth
+    queryset = BarberModel_Auth.objects.comments.all() #applied no filters yet!
+    def get_context_data(self , **kwargs):
+        data = super().get_context_data(**kwargs)
+        connected_comments = Comment.objects.filter(CommentPost=self.get_object())
+        number_of_comments = connected_comments.count()
+        data['comments'] = connected_comments
+        data['no_of_comments'] = number_of_comments
+        # data['comment_form'] = CommentForm()
+    
