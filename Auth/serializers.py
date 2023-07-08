f04828a833d7ddcea0f24d68c07431761529a09b
AmirMohammadKomijani <78591495+AmirMohammadKomijani@users.noreply.github.com>
2023-07-08 12:57:29 +0330

    fixing profile problem

--- Auth/serializers.py
+++ Auth/serializers.py
@@ -20,7 +20,7 @@ class UserCreateSerializer(BaseUserCreateSerializer):
 
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
-        fields = ['email','username','password']
+        fields = ['username','password']
 
 
 
